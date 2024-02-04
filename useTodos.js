a82eaaf36a14a6e3b1a5e25d1422a9ee8cfda4f7
[Diego] <[diegoemanuel134@gmail.com>
2024-02-04 09:21:45 -0600

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,53 @@
+import { useReducer, useEffect } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer"
+
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodos = ( initialState = [] ) => {
+    
+    const [ todos , dispatch ] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify( todos ));
+    
+    }, [todos])
+   
+    const handleNewTodo = (todo) => {
+        const action = {
+            type : '[TODO] Add Todo',
+            payload : todo
+        }
+
+        dispatch( action ); //Se manda la accion
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload : id
+        })
+    }
+
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload : id
+        })
+    }
+
+    // const todosCount = todos.length;
+    // const pendingTodosCount = todos.filter(( todo) => !todo.done).length;
+ 
+    return {
+        todos,
+        todosCount : todos.length,
+        pendingTodosCount :  todos.filter(( todo) => !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo
+    }
+
+}
